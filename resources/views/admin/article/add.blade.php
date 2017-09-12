bbc9d6822e79bf8c2fadc2722f0f8bbbf84eff93
victor24680 <490319148@qq.com>
2017-09-12 23:10:18 +0800

    修订版本

--- resources/views/admin/article/add.blade.php
+++ resources/views/admin/article/add.blade.php
@@ -90,7 +90,7 @@
                             <script type="text/javascript" charset="utf-8" src="{{asset('extend/ueditor/ueditor.config.js')}}"></script>
                             <script type="text/javascript" charset="utf-8" src="{{asset('extend/ueditor/ueditor.all.min.js')}}"> </script>
                             <script type="text/javascript" charset="utf-8" src="{{asset('extend/ueditor/lang/zh-cn/zh-cn.js')}}"></script>
-                            <script id="editor" type="text/plain" style="width:1024px;height:500px;"></script>
+                            <script id="editor" name="art_content" type="text/plain" style="width:1024px;height:500px;"></script>
                             <script>
                                 var ue = UE.getEditor('editor');
                             </script>
@@ -99,7 +99,7 @@
                                 div .edui-combox,div .edui-button-body,div .edui-splitbutton-body{overflow:hidden;height:20px;}
                                 div .edui-box{overflow:hidden;height:22px;}
                             </style>
-                            <textarea name="art_content" >{{old('art_content')}}</textarea>
+                            <!--<textarea name="art_content" >{{old('art_content')}}</textarea>-->
                         </td>
                     </tr>
                     <tr>
